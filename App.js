641bba8c86b1419ac1aaf0ca3feff79b202a7104
malikkorucu <58563002+malikkorucu@users.noreply.github.com>
2021-12-28 08:49:36 +0300

    first commit

---
+++ App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import {SafeAreaView, Text} from 'react-native';
+
+const App = () => {
+  return (
+    <SafeAreaView style={{flex:1 , alignItems:'center' , justifyContent:'center'}}>
+      <Text>HELLO WORLD</Text>
+    </SafeAreaView>
+  );
+};
+
+export default App;

12559a54b9aa0da04bb2121fec8ce1b2321ccdba
Malik Korucu <58563002+malikkorucu@users.noreply.github.com>
2022-01-02 21:54:15 +0300

    Custom Flat list component added

--- src/components/FlatList/index.tsx
+++ src/components/FlatList/index.tsx
@@ -1,6 +1,8 @@
 import React, {FC} from 'react';
 import {FlatList as F, ListRenderItem} from 'react-native';
 
+// Custom Flatlist
+
 type Props = {
   data: Array<any>;
   renderItem: ListRenderItem<any>;

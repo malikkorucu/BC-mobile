c6c95e7bb2b99c4964d53a2b2cb93118354fdef1
malikkorucu <58563002+malikkorucu@users.noreply.github.com>
2022-01-10 17:52:58 +0300

    Bottom Sheet and Date Time picker components added

--- src/screens/Home/HomeScreen.tsx
+++ src/screens/Home/HomeScreen.tsx
@@ -1,44 +1,66 @@
-import React, {FC, useLayoutEffect} from 'react';
+import React, {FC, Ref, useLayoutEffect, useState} from 'react';
 import {Button, StatusBar, Text, View} from 'react-native';
-import {NavigationProp, useNavigation} from '@react-navigation/native';
-import {TextInput} from '@components';
+import {useNavigation} from '@react-navigation/native';
 import layout from '../../config/layout.json';
-import { useEffect } from 'react';
+import {useEffect, useRef} from 'react';
+import {DateTimePicker} from './../../components/DateTimePicker/DateTimePicker';
+import BottomSheet from './../../components/BottomSheet/BottomSheet';
 
 export const HomeScreen: FC<any> = () => {
   const navigation: any = useNavigation();
+  const [modalVisible, setModalVisible] = useState(false);
+  const bottomsheet = useRef() as any;
 
-  useEffect(()=> {
+  useEffect(() => {
     StatusBar.setBarStyle('dark-content');
-  },[])
+  }, []);
 
-  // useLayoutEffect(() => {
-  //   if (layout.menu === 'drawer') {
-  //     navigation.setOptions({
-  //       headerRight: () => (
-  //         <Button title="Drawer" onPress={() => navigation.openDrawer()} />
-  //       ),
-  //     });
-  //   }
-  // }, [navigation]);
+  useLayoutEffect(() => {
+    if (layout.menu === 'drawer') {
+      navigation.setOptions({
+        headerRight: () => (
+          <Button title="Drawer" onPress={() => navigation.openDrawer()} />
+        ),
+      });
+    }
+  }, [navigation]);
 
   return (
-   <>
-    <View
-      style={{
-        flex: 1,
-        alignItems: 'center',
-        justifyContent: 'center',
-        padding: 16,
-        backgroundColor:'white'
-      }}>
-      <Button
-        title="Home Detail"
-        onPress={() => navigation.navigate('HomeDetail')}
-      />
-      <TextInput />
-      <Text>{JSON.stringify(navigation.getState(), null, 2)}</Text>
-    </View>
-   </>
+    <>
+      <View
+        style={{
+          flex: 1,
+          padding: 16,
+          backgroundColor: 'white',
+        }}>
+        <View style={{marginBottom: 5}}>
+          <Button
+            title="Home Detail"
+            onPress={() => navigation.navigate('HomeDetail')}
+          />
+        </View>
+        <View style={{marginBottom: 5}}>
+          <Button
+            title="Show Date Picker"
+            onPress={() => setModalVisible(!modalVisible)}
+          />
+        </View>
+        <View style={{marginBottom: 5}}>
+          <Button
+            title="Bottom Sheet"
+            onPress={() => bottomsheet.current.open()}
+          />
+        </View>
+        <DateTimePicker visible={modalVisible} setVisible={setModalVisible} />
+        <BottomSheet
+          ref={bottomsheet}
+          height={500}
+          openDuration={300}
+          closeOnDragDown
+          closeOnPressMask>
+          <Text>aslşdkfjasşldfkj</Text>
+        </BottomSheet>
+      </View>
+    </>
   );
 };
